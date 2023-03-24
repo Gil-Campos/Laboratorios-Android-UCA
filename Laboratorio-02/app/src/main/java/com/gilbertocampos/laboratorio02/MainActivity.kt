deba85f7a174d9db5526f86e4dfce60aa6eb5bb1
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2023-03-24 10:56:33 -0600

    Finish product

--- Laboratorio-02/app/src/main/java/com/gilbertocampos/laboratorio02/MainActivity.kt
+++ Laboratorio-02/app/src/main/java/com/gilbertocampos/laboratorio02/MainActivity.kt
@@ -2,10 +2,40 @@ package com.gilbertocampos.laboratorio02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var weight: EditText
+    private lateinit var height: EditText
+    private lateinit var calculate: Button
+    private lateinit var healthResult: TextView
+    private lateinit var bmiHealthResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bind()
+
+        calculate.setOnClickListener {
+            calculateBMI()
+            healthResult.text = calculateBMI().toString()
+        }
+    }
+
+    private fun bind() {
+        weight = findViewById(R.id.weight_editText)
+        height = findViewById(R.id.height_editText)
+        calculate = findViewById(R.id.btn_calc_bmi)
+        healthResult = findViewById(R.id.bmi_result)
+        bmiHealthResult = findViewById(R.id.bmi_health_result)
+    }
+
+    private fun calculateBMI(): Float {
+        val userWeight = weight.text.toString().toFloat()
+        val userHeight = height.text.toString().toFloat()
+        return (userWeight) / (userHeight * userHeight) * 10000
     }
 }
