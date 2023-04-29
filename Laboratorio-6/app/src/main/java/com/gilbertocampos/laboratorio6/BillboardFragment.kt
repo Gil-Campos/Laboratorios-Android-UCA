6638413762f4b53a9bb216816d2ff643546392b3
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2023-04-28 18:01:56 -0600

    Finish the the UI and actions of the billboard fragment of lab06

--- Laboratorio-6/app/src/main/java/com/gilbertocampos/laboratorio6/BillboardFragment.kt
+++ Laboratorio-6/app/src/main/java/com/gilbertocampos/laboratorio6/BillboardFragment.kt
@@ -6,12 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class BillboardFragment : Fragment() {
 
-    private val viewModel: MovieViewModel by activityViewModels{ MovieViewModel.Factory }
+    private val viewModel: MovieViewModel by activityViewModels { MovieViewModel.Factory }
+    private lateinit var seeMovie: CardView
+    private lateinit var fabAddMovie: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +28,23 @@ class BillboardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        bind(view)
+        actions()
         Log.d("Movie", viewModel.getMovies().toString())
+    }
+
+    private fun bind(view: View) {
+        fabAddMovie = view.findViewById(R.id.fabAddMovie)
+        seeMovie = view.findViewById(R.id.star_wars_card)
+    }
+
+    private fun actions() {
+        fabAddMovie.setOnClickListener {
+            it.findNavController().navigate(R.id.newMovieFragment)
+        }
+
+        seeMovie.setOnClickListener {
+            it.findNavController().navigate(R.id.movieFragment)
+        }
     }
 }
