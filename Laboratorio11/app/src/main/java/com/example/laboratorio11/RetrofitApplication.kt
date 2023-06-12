864d8658a44af436abcc0b3323838dfb1fe490e9
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2023-06-11 18:56:30 -0600

    Finish lab 11

---
+++ Laboratorio11/app/src/main/java/com/example/laboratorio11/RetrofitApplication.kt
@@ -0,0 +1,38 @@
+package com.example.laboratorio11
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import com.example.laboratorio11.network.retrofit.RetrofitInstance
+import com.example.laboratorio11.network.service.AuthService
+import com.example.laboratorio11.repository.CredentialsRepository
+
+class RetrofitApplication : Application() {
+
+    // TODO: Create a variable to store the token in the SharedPreferences
+    private val prefs: SharedPreferences by lazy {
+        getSharedPreferences("Retrofit", Context.MODE_PRIVATE)
+    }
+
+    // TODO: Get the API service from the Retrofit instance and set the token
+    private fun getApiService() = with(RetrofitInstance) {
+        setToken(getToken())
+        getLoginService()
+    }
+
+    fun getToken(): String = prefs.getString(USER_TOKEN, "")!!
+
+    val credentialsRepository: CredentialsRepository by lazy {
+        CredentialsRepository(getApiService())
+    }
+
+    fun saveAuthToken(token: String) {
+        val editor = prefs.edit()
+        editor.putString(USER_TOKEN, token)
+        editor.apply()
+    }
+
+    companion object {
+        const val USER_TOKEN = "user_token"
+    }
+}
